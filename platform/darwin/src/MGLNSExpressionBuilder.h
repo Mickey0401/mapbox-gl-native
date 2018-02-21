c89a9bc6bba1f8eb4c0f7ba51a14301c2d84f03d
Fabian Guerra <fabian.guerra@mapbox.com>
2018-02-21 18:22:09 -0500

    [ios, macos] Add NSExpression constant value concatenation.

---
+++ platform/darwin/src/MGLNSExpressionBuilder.h
@@ -0,0 +1,13 @@
+//
+//  MGLNSExpressionBuilder.h
+//  ios
+//
+//  Created by Fabian Guerra Soto on 2/21/18.
+//  Copyright © 2018 Mapbox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MGLNSExpressionBuilder : NSObject
+
+@end
---
+++ platform/darwin/src/MGLNSExpressionBuilder.m
@@ -0,0 +1,13 @@
+//
+//  MGLNSExpressionBuilder.m
+//  ios
+//
+//  Created by Fabian Guerra Soto on 2/21/18.
+//  Copyright © 2018 Mapbox. All rights reserved.
+//
+
+#import "MGLNSExpressionBuilder.h"
+
+@implementation MGLNSExpressionBuilder
+
+@end
